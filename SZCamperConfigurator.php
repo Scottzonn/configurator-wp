f21bc04c37865adfb18830a3a8ebceed14b7d6ca
Scottzonn <scottzonneveldt@gmail.com>
2022-01-08 04:24:31 +1100

    update build

--- SZCamperConfigurator.php
+++ SZCamperConfigurator.php
@@ -5,7 +5,7 @@
     Description:    Camper configurator for Australian brands
     Author:         Scott Zonneveldt
     Author URI:     http://webcrunch.com.au
-    Version:        1.0.21
+    Version:        1.0.23
 */
 
 define( 'WP_DEBUG', true );
