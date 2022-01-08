1d5169278e9b9316e45983e04312802d2e1b2138
Scottzonn <scottzonneveldt@gmail.com>
2022-01-09 02:05:48 +1100

    fix debux

--- SZCamperConfigurator.php
+++ SZCamperConfigurator.php
@@ -83,6 +83,7 @@ class SZCamperConfigurator {
 	public function route_send_email(WP_REST_Request $request){
 		//gets parsed params
 		$json = $request->get_json_params();
+		echo print_r($this, true);
 		echo print_r($this->email_notifications, true);
 		$this->email_notifications->sz_sendmail($json, 'self');
 	}
