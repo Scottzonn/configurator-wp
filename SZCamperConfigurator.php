06b06cbd458ec322dcc788ad7af48b18ec328897
Scottzonn <scottzonneveldt@gmail.com>
2022-02-13 06:15:56 +1100

    change cors

--- SZCamperConfigurator.php
+++ SZCamperConfigurator.php
@@ -37,7 +37,27 @@ function rp_load_plugin(){
 
 add_action('init','rp_load_plugin');
 
+add_action( 'rest_api_init', 'wp_rest_allow_all_cors', 15 );
 
+/**
+ * Allow all CORS.
+ *
+ * @since 1.0.0
+ */
+function wp_rest_allow_all_cors() {
+    // Remove the default filter.
+    remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+
+    // Add a Custom filter.
+    add_filter( 'rest_pre_serve_request', function( $value ) {
+        // header( 'Access-Control-Allow-Origin: *' );
+		header( 'Access-Control-Allow-Origin: ' . esc_url_raw( site_url() ) );
+        header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE' );
+        header( 'Access-Control-Allow-Credentials: true' );
+        header( 'Access-Control-Allow-Headers: *' );
+        return $value;
+    });
+}
 /**
  * Class SZCamperConfigurator.
  */
