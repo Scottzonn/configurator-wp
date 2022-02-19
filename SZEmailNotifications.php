218ed3cb756769ef06b023b76f9da38fc8bb8d02
Scottzonn <scottzonneveldt@gmail.com>
2022-02-20 07:11:41 +1100

    add html boilerplate to email

--- SZEmailNotifications.php
+++ SZEmailNotifications.php
@@ -19,7 +19,16 @@ class SZEmailNotifications{
 			"Reply-To: {$userEmailFields['replyTo']}"
 		);
 
-		if(wp_mail($userEmailFields['emailTo'], $userEmailFields['emailSubject'], $userEmailFields['emailTemplate'], $headers)) {
+
+		$HTMLheader = '<html xmlns="http://www.w3.org/1999/xhtml">
+		<head>
+			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		</head><body>';
+
+		$HTMLfooter = '</body></html>';
+
+		if(wp_mail($userEmailFields['emailTo'], $userEmailFields['emailSubject'], $HTMLheader . $userEmailFields['emailTemplate'] . $HTMLfooter, $headers)) {
 			$response = [
 				message => 'Email Sent',
 				success => true,
